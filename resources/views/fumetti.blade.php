e7f68fa0ed8df3bd8342c11e83c68f02a4046d17
Leo9723 <pala9761@gmail.com>
2023-02-20 17:25:20 +0100

    correzione nome rotta

--- resources/views/fumetti.blade.php
+++ resources/views/fumetti.blade.php
@@ -4,7 +4,7 @@
 
 <div class="main-cont">
    @foreach ($comics as $key => $comic)
-   <a href="{{ route('detail-comic', ['id' => $comic['id']]) }}">
+   <a href="{{ route('detail-fumetti', ['id' => $comic['id']]) }}">
       <div class="card">
          <div class="thumb">
             <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
@@ -18,25 +18,4 @@
 </div>
 @endsection('content')
 
-<style lang="scss">
-   .main-cont{
-      width: 80%;
-      margin: 0 auto;
-      display: flex;
-      flex-wrap: wrap;
-      padding: 100px 0px;
-      
-   }
-   a{
-      width: calc(100% / 7);
-      margin: 10px;
-   }
-   .card img{
-      width: 100%;
 
-   }
-   .main-cont a {
-      text-decoration: none;
-      color: black;
-   }
-</style>
